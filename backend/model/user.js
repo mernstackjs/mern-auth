1a41309124265a6ba952ca69ba95072272f26828
learncodewithahmed <learncodewithahmed@example.com>
2025-09-06 13:44:35 +0200

    i modify userschema

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require("mongoose");
-
+const argon = require("argon2");
 const userSchema = new Schema(
   {
     name: String,
@@ -10,6 +10,22 @@ const userSchema = new Schema(
     timeseries: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  try {
+    const hashedPassword = await argon.hash(this.password, 12);
+    this.password = hashedPassword;
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function () {
+  return argon.verify(this.password, password);
+};
 
 const User = model("User", userSchema);
 module.exports = User;
