f555e56014c8fb9c8e41ec39e9f1aa57c77a2b66
learncodewithahmed <learncodewithahmed@example.com>
2025-09-06 13:54:14 +0200

    i create index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,27 @@
-console.log("i am server");
+require("dotenv");
+const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (_req, res) => {
+  res.json({
+    message: `Server is running on  ${PORT}`,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
